7db4bc4bd9a2babe55364598f94c610de2348833
fawda123 <mbafs2012@gmail.com>
2024-08-01 15:59:47 -0400

    add skeleton for other modules, change permissions on ci

--- R/agenda.R
+++ R/agenda.R
@@ -2,7 +2,11 @@ library(tibble)
 
 agenda <- frame_matrix(
   ~Time, ~Topic,
-   '10:00', '[Introduction](intro.qmd)',
+   '10:00', '[R Basics](rbasics.qmd)',
+   '11:00', '[Data Preparation](dataprep.qmd)',
+   '12:00', 'Lunch',
+   '1:00', '[Using EBASE](ebase.qmd)',
+   '2:00', '[Interpreting Results](interpret.qmd)',
    '3:00', 'adjourn'
 )
 
