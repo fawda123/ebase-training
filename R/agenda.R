9a29f305f2b9ab9c5ee098fee03f07350ba323cf
fawda123 <mbafs2012@gmail.com>
2024-08-08 16:07:08 -0400

    rework template

--- R/agenda.R
+++ R/agenda.R
@@ -2,11 +2,11 @@ library(tibble)
 
 agenda <- frame_matrix(
   ~Time, ~Topic,
-   '10:00', '[R Basics](rbasics.qmd)',
-   '11:00', '[Data Preparation](dataprep.qmd)',
-   '12:00', 'Lunch',
-   '1:00', '[Using EBASE](ebase.qmd)',
-   '2:00', '[Interpreting Results](interpret.qmd)',
+   '10:00', '[Introduction](01_intro.qmd)',
+   '10:30', '[Data Preparation](02_dataprep.qmd)',
+   '11:30', 'Break',
+   '12:00', '[Using EBASE](03_ebase.qmd)',
+   '1:30', '[Interpreting Results](04_interpret.qmd)',
    '3:00', 'adjourn'
 )
 
