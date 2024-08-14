31b355a0b3abe8c16bda04bd33c800f8f9c4f59c
fawda123 <mbafs2012@gmail.com>
2024-08-14 15:19:57 -0400

    minor edits to intro, add some refs

--- R/agenda.R
+++ R/agenda.R
@@ -7,7 +7,7 @@ agenda <- frame_matrix(
    '11:30', 'Break',
    '12:00', '[Using EBASE](03_ebase.qmd)',
    '1:30', '[Interpreting Results](04_interpret.qmd)',
-   '3:00', 'adjourn'
+   '3:00', 'Adjourn'
 )
 
 save(agenda, file = 'data/agenda.RData')
